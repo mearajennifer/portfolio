8b501b550d5f34879db940d3c1bf7f4af8fe5d4a
Jennifer Meara <jenniferlmeara@gmail.com>
2018-07-22 22:51:11 +0000

    Create basic server for web app, write html templates, and style css

--- server.py
+++ server.py
@@ -23,14 +23,11 @@ def index():
 
 
 if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the
-    # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    # app.debug = True
 
     # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+    # app.jinja_env.auto_reload = app.debug
 
-    # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
-    app.run(port=5000, host='0.0.0.0')
+    app.run()
